8b03091193c8638ed4b7d175fd73892405ea1b54
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-19 15:42:01 +0000

    adding the post route

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(cors())
+app.use(bodyParser.json());
 
-v
-app.get("/", (req, res) => {
-  res.send("this is working");
-});
-app.listen(3000, () => {
-  console.log("app is running on port 3000 :");
-});
+app.get('/', (req, res)=> {
+  res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})

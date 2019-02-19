2a56ebb057989867618104b5a580cb1500503091
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-19 15:52:03 +0000

    adding the get route with new params

--- server.js
+++ server.js
@@ -55,3 +55,17 @@ app.post('/register', (req, res) => {
       })
       .catch(err => res.status(400).json('unable to register'))
   })
+  app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({id})
+      .then(user => {
+        if (user.length) {
+          res.json(user[0])
+        } else {
+          res.status(400).json('Not found')
+        }
+      })
+      .catch(err => res.status(400).json('error getting user'))
+  })
+
+  

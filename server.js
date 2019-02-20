4c1853b98bd55fce564fe29ace2ed7b38d014b46
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-20 15:47:17 +0000

    retreving data from database with knex

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const knex = require('knex')({
-    client: 'mysql',
+    client: 'pg',
     connection: {
       host : '127.0.0.1',
-      user : 'your_database_user',
-      password : 'your_database_password',
-      database : 'myapp_test'
+      user : 'postgres',
+      password : '',
+      database : 'smart-brain'
     }
   });
+
+
+
+
+ console.log(knex.select('*').from('users')); 
+
 
 const database = {
   users: [{
